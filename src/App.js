b9858f5b0e6b9854a67d8a8e0a4571cd4663a49d
mayanksh99 <mayankshakya992@gmail.com>
2020-05-14 13:02:57 +0530

    --init:simple darkmode

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyles } from "./global";
+import "./App.css";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  // The function that toggles between themes
+  const toggleTheme = () => {
+    // if the theme is not light, then set it to dark
+    if (theme === "light") {
+      setTheme("dark");
+      // otherwise, it should be light
+    } else {
+      setTheme("light");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <button onClick={toggleTheme}>Toggle theme</button>
+        <h1>It's a light theme!</h1>
+        <footer></footer>
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
